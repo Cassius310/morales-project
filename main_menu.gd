7129aad368ac0c40e6385ef35d3bffd252e50900
Cassius310 <cmorales@usv.edu>
2026-02-11 14:03:40 -0800

    polish
    polish

--- main_menu.gd
+++ main_menu.gd
@@ -5,13 +5,13 @@ func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
-func _on_continue_pressed() -> void:
+func _on_continue_pressed():
 	pass # Replace with function body.
 
 
-func _on_settings_pressed() -> void:
+func _on_settings_pressed():
 	pass # Replace with function body.
 
 
-func _on_quit_pressed() -> void:
+func _on_quit_pressed():
 	get_tree().quit()
